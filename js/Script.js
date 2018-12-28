c32556838e172f37a5d8369b4d855e9d02768284
Yelnur17 <46210553+Yelnur17@users.noreply.github.com>
2018-12-28 11:31:21 +0600

    Add files via upload

---
+++ js/Script.js
@@ -0,0 +1,9 @@
+$(function(){
+
+	$('a[data-target^="anchor"]').bind('click.smoothscroll',function(){
+		var target = $(this).attr('href'),
+		bl_top = $(target).offset().top - 100;
+		$('body, html').animate({scrollTop: bl_top}, 700);
+		return false;
+	})
+});
